b5a5f93d9fc433a2f2e56d44d9ebb7ee4f4e547c
Basss2ss <68554683+Basss2ss@users.noreply.github.com>
2020-07-20 18:33:43 +0300

    copmany
    initial commit

---
+++ js/main.js
@@ -0,0 +1,20 @@
+$(function(){
+
+    $('.slider__inner, .news__slired-inner').slick({
+        nextArrow: '<button type="button" class="slick-btn slick-next"></button>',
+        prevArrow: '<button type="button" class="slick-btn slick-prev"></button>',
+        infinite: false
+    });
+
+    $('.header__btn-menu').on('click', function(){
+        $('.menu ul').slideToggle();
+
+    });
+
+    
+
+
+
+
+
+});
